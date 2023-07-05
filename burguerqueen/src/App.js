5a06de606e92ccb2ed56bfdb5610e90e2aa1cd42
Gaabs <ggaabrielagomez@gmail.com>
2023-07-05 00:21:31 -0600

    Agregar paths al router

--- burguerqueen/src/App.js
+++ burguerqueen/src/App.js
@@ -1,12 +1,22 @@
+import React from 'react';
 import './assetss/sass/app.sass';
+
+import { BrowserRouter as Router , Switch, Route } from 'react-router-dom'
+
+import Login from './components/login';
+import Dashboard from './components/dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hola, mundo</p>
-      </header>
-    </div>
+   <React.Fragment>
+      <Router>
+        <Switch>
+            <Login />
+          <Route path='/' exact render= { props => ( <Login {...props} />) }> </Route>
+          <Route path='/menu' exact render= { props => ( <Dashboard {...props} />) }> </Route>
+        </Switch>
+      </Router>
+   </React.Fragment>
   );
 }
 
