5733725ed2b5bde5abef8f13fd57ae3cf358988f
Gaabs <ggaabrielagomez@gmail.com>
2023-07-05 22:11:05 -0600

    arreglo de router para mostrar paths exactos

--- burguerqueen/src/App.js
+++ burguerqueen/src/App.js
@@ -4,16 +4,15 @@ import './assetss/sass/app.sass';
 import { BrowserRouter as Router , Switch, Route } from 'react-router-dom'
 
 import Login from './components/login';
-import Dashboard from './components/dashboard';
+import Dashboard from './components/dashboard.jsx';
 
 function App() {
   return (
    <React.Fragment>
       <Router>
         <Switch>
-            <Login />
-          <Route path='/' exact render= { props => ( <Login {...props} />) }> </Route>
-          <Route path='/menu' exact render= { props => ( <Dashboard {...props} />) }> </Route>
+          <Route exact path='/' component= {Login} />
+          <Route exact path='/dashboard'component= {Dashboard} />
         </Switch>
       </Router>
    </React.Fragment>
